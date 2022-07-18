060eead65ec5686a2a361bb647526671fed83502
jojoxjohn <86195023+jojoxjohn@users.noreply.github.com>
2022-07-18 00:38:09 -0500

    conexionBD.php

---
+++ shared/conexionBD.php
@@ -0,0 +1,25 @@
+<?php
+class conexionBD
+{
+    static private $instancia = null;
+    public $conexion = null;
+
+    private function __construct()
+    {
+        $connection = "mysql:host=" . "localhost" . ";dbname=" . "desarrollo_web" . ";charset=" . "utf8mb4";
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+        $pdo = new PDO($connection, "root", "", $options);
+        $this->conexion = $pdo;
+    }
+
+    public static function obtenerInstancia()
+    {
+        if (self::$instancia == null)
+            self::$instancia = new conexionBD();
+        return (self::$instancia);
+    }
+}
