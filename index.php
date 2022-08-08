1e12c7de55b3d777a67fa29fb878b41e9da9bf96
Germán Huaytalla <ivanhuaytalla8@gmail.com>
2022-08-07 20:01:47 -0500

    imple carrito + bd

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
-	include_once("view/formHistorialCompras.php");
-	include_once("model/modelOrdenCompra.php");
+	// include_once("view/formHistorialCompras.php");
+	// include_once("model/modelOrdenCompra.php");
 
-	$arr = (new ModelOrdenCompra())->obtenerOrdenCompra();
+	// $arr = (new ModelOrdenCompra())->obtenerOrdenCompra();
 	
-	$obj = new FormHistorialCompras();
-	$obj -> showFormHistorialCompras($arr);
+	// $obj = new FormHistorialCompras();
+	// $obj -> showFormHistorialCompras($arr);
+
+
+	include_once('view/formLoginUsuario.php');
+	$objForLoginUsuario = new formLoginUsuario();
+	$objForLoginUsuario -> formLoginUsuarioShow();
+?>
